c44651ef3c8c0e6c5f87b8ec4d4a28a69f5cfc3b
eliashz <eliashz@gmail.com>
2023-01-27 11:51:07 +0100

    1 de 50

--- src/components/organisms/ITATable/index.jsx
+++ src/components/organisms/ITATable/index.jsx
@@ -1,9 +1,19 @@
+import styled from 'styled-components'
 import { useEffect, useContext } from 'react'
 import TableProvider, { TableContext } from './store/context'
 import { Actions } from './store/reducer'
-import { TableStyled } from './styles'
+import { TableCell, TableStyled } from './styles'
 import TableBody from './TableBody'
 import TableHeader from './TableHeader'
+import { Icon, Text } from '../../atoms'
+import { FlexBox } from '../../../styles'
+import { colors } from '../../../styles'
+
+const StyledIcon = styled(Icon)`
+  color: ${colors.purple};
+  font-size: 1.5em;
+  cursor: pointer;
+`
 
 function Table({ columns, data, showHeader = true }) {
   const { dispatch } = useContext(TableContext)
@@ -17,6 +27,15 @@ function Table({ columns, data, showHeader = true }) {
     <TableStyled>
       {showHeader && <TableHeader />}
       <TableBody />
+      <TableCell>
+        <FlexBox direction="row" align="center">
+          <StyledIcon icon="arrow_back_ios" wght="800" />
+          <Text as="b" fontSize=".7em">
+            Página 1 de 50
+          </Text>
+          <StyledIcon icon="arrow_forward_ios" wght="800" />
+        </FlexBox>
+      </TableCell>
     </TableStyled>
   )
 }
