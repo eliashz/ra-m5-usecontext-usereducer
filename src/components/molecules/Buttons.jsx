45fd6649859f5a9061ec569d4e8bc5c5ac74ad2a
eliashz <eliashz@gmail.com>
2023-01-30 16:14:44 +0100

    proptypes

--- src/components/molecules/Buttons.jsx
+++ src/components/molecules/Buttons.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { colors, FlexBox } from '../../styles'
 import { Button, Icon } from '../atoms'
@@ -16,7 +17,7 @@ const StyledIcon = styled(Icon)`
   padding-right: 2px;
 `
 
-function Buttons({columns, data}) {
+function Buttons({ columns, data }) {
   return (
     <FlexBox direction="row" justify="flex-end">
       <StyledButton
@@ -35,12 +36,38 @@ function Buttons({columns, data}) {
       >
         Por barrio
       </StyledButton>
-      <StyledButton background={colors.green} boxShadow="none" onClick={() => downloadTable(columns, data)}>
+      <StyledButton
+        background={colors.green}
+        boxShadow="none"
+        onClick={() => downloadTable(columns, data)}
+      >
         <StyledIcon icon="download" />
         Descargar
       </StyledButton>
     </FlexBox>
   )
+}
+
+Buttons.propTypes = {
+  data: PropTypes.objectOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      city: PropTypes.string.isRequired,
+      district: PropTypes.string.isRequired,
+      createdAt: PropTypes.string,
+      updatedAt: PropTypes.string,
+    }),
+  ),
+  columns: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+    }),
+  ),
 }
 
 export default Buttons
